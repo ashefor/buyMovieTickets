bf163b876178836abe741b240ecc3150e67a37b9
ashefor <thatsureboy@gmail.com>
2019-05-23 01:08:14 +0100

    set up folder structure and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routeComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    routeComponents
   ],
   imports: [
     BrowserModule,
